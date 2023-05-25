5fefedb149011ec816e66f6a178b855e3e49ca9b
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-05-24 20:59:05 -0500

    refactor: Split workspace into library crates
    The `cli` crate no longer contains the utility functionality. Instead it
    is contained in the `cheesecalc` crate and imported where it is used.
    There is now a `wasm` crate which exports functions that can be used in
    a web context.

---
+++ crates/wasm/src/lib.rs
@@ -0,0 +1,20 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+fn init_wasm() -> Result<(), JsValue> {
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn from_cheese(cheese_mass: f64) -> Result<JsValue, JsValue> {
+    let items = cheesecalc::from_cheese_mass(cheese_mass);
+
+    serde_wasm_bindgen::to_value(&items).map_err(|e| e.into())
+}
+
+#[wasm_bindgen]
+pub fn from_pasta(pasta_mass: f64) -> Result<JsValue, JsValue> {
+    let items = cheesecalc::from_pasta_mass(pasta_mass);
+
+    serde_wasm_bindgen::to_value(&items).map_err(|e| e.into())
+}
